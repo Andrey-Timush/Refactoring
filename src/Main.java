8dc87b295cb289f20f9277b24f71edaedbe565b7
Andrey <andrikshumit@gmail.com>
2020-06-17 17:07:48 +0300

    Checked mistakes

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,10 @@ interface DataConnection {
 
 class MyApp implements DataConnection {
 
-    public static class MyAppFactory {
         public static MyApp create(String y) {
             return new MyApp(y);
         }
-    }
+
     public MyApp(String y) {
         this.y = y;
     }
@@ -28,15 +27,13 @@ class MyApp implements DataConnection {
             System.out.println("app v.1.13");
             for (int i = startYear; i < endYear; i++) {
                 int sum = 0;
-                COUNT = 0;
-                COUNT1 = 0;
                 String y = i + "";
-                sum = new MyAppFactory().create(y).loadDatas(sum);
+                sum =  create(y).loadDatas(sum);
                 double qq = sum > 0 ? (double) sum / (double) COUNT : 0;
                 if (qq > 0) {
                     System.out.println(i + " " + qq);
                 }
-                new /*new*/ MyAppFactory().create(y).saveData(i, (int) qq);
+                 /*new*/ create(y).saveData(i, (int) qq);
             }
             System.out.println("gotovo");
         } catch (Exception e) {
@@ -45,40 +42,30 @@ class MyApp implements DataConnection {
     }
 
     public int loadDatas(int sum) throws Exception {
-/*
-        File file = new File("1.txt");
-*/
 //try {
 //FileOutputStream fis = new FileOutputStream(file);
         FileInputStream fis = new FileInputStream("1.txt");
-        String s = "";
+        StringBuilder s = new StringBuilder();
         int i = fis.read();
-        while (i != -1){
-            s = s + new String(new byte[]{(byte) i});
+        do {
+            s.append(new String(new byte[]{(byte) i}));
             i = fis.read();
-        }
-
-        /*do {
-            s = s + new String(new byte[]{(byte) i});
-            i = fis.read();
-        } while (i != -1);*/
-
+        } while (i != -1);
         int begin = 0;
         while (true) {
-            int e = s.indexOf(s, begin + 1);
+            int e = s.indexOf("\n", begin + 1);
             if (e == -1) {
                 break;
             }
             String ss = s.substring(begin, e);
-//System.out.println(ss);
+            System.out.println("ss="+ss);
             String[] sss = ss.split(" ");
-            for (String string : sss) {
-//System.out.println(string);
-            }
+//            for (String string : sss) {
+//                System.out.println("asd = "+string);
+//            }
             if (sss[2].contains(this.y) /*|| sss[2].contains(y)*/) {
                 sum = sum + Integer.parseInt(sss[3]);
             }
-
             COUNT++;
             begin = e;
         }
@@ -86,7 +73,7 @@ class MyApp implements DataConnection {
     }
     public void saveData(int year, int qq) throws IOException {
         FileOutputStream fis = new FileOutputStream("statistika.txt", true);
-        String s;
+        String s = "";
         s = COUNT1 + " " + year + " " + qq + "\n";
         fis.write(s.getBytes());
         COUNT1++;
